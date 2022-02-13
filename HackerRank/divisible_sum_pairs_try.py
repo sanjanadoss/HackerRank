4ba4273ac882a13861b399053b5bcf831e486352
sanjanadoss <sanjanadoss2503@gmail.com>
2022-02-14 00:27:13 +0530

    13-02/1

---
+++ HackerRank/divisible_sum_pairs_try.py
@@ -0,0 +1,45 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar): #4 loops, worst-case time complexity 
+    y = []
+    for i in range(n):
+        for j in range(n):
+            if ar[i]>ar[j]:
+                if (ar[i]+ar[j])%k == 0:
+                    y.append(i)
+                    y.append(j)
+    return int(len(y)/2)
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
