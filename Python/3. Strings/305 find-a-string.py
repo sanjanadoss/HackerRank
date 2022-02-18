45a042d20030c6b879682d98ea3a0e280d490847
sanjanadoss <sanjanadoss2503@gmail.com>
2022-02-18 14:50:50 +0530

    Create 305 find-a-string.py

---
+++ Python/3. Strings/305 find-a-string.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/find-a-string/submissions/code/256283821
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
