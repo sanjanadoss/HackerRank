96cd39fdbb6959458425cb9c0592ba9721887f43
sanjanadoss <sanjanadoss2503@gmail.com>
2022-02-20 22:25:44 +0530

    Update 20-02

---
+++ C++/1. Introduction/102 input-and-out.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout << x+y+z;
+    return 0;
+}
