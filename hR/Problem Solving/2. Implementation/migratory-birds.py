91dbf8405892eb88d569966bf2cf1f5c54fc139d
sanjanadoss <sanjanadoss2503@gmail.com>
2022-02-27 00:02:51 +0530

    Update migratory-birds.py

--- hR/Problem Solving/2. Implementation/migratory-birds.py
+++ hR/Problem Solving/2. Implementation/migratory-birds.py
@@ -1,7 +1,20 @@
 #https://www.hackerrank.com/challenges/migratory-birds/problem
 
-input()
-count = [0]*6
-for t in map(int,input().strip().split()):
-    count[t] += 1
-print(count.index(max(count)))
+def migratoryBirds(arr):
+    count = [0]*len(arr)
+    for i in arr:
+        count[i] += 1
+    return count.index(max(count))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
