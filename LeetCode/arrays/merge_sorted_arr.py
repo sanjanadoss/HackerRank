c63c5b78a957566c1b02af638f3c92febe2339e3
sanjanadoss <sanjanadoss2503@gmail.com>
2022-09-15 11:00:08 +0530

    Update merge_sorted_arr.py

--- LeetCode/arrays/merge_sorted_arr.py
+++ LeetCode/arrays/merge_sorted_arr.py
@@ -1,17 +1,21 @@
 #https://leetcode.com/problems/merge-sorted-array/
+def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        a, b, trav_p = m-1, n-1, m + n - 1
 
-       #Adding elements to nums1 from the back
-        last = m+n-1
-        while m>0 and n>0:
-            if nums1[m-1]>nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m -= 1
-            else:
-                nums1[last] = nums2[n-1]
-                n -= 1
-            last -= 1
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]: 
+                nums1[trav_p] = nums1[a]
+                a -= 1
+            else: #handles edge case where there are remaining elements 
+                #in b and they just have to be appended in a
+                nums1[trav_p] = nums2[b]
+                b -= 1
 
-#edge case where we just append the remaining elements in num2, to the beginning of nums1 since all the bigger elements were already added at the end. 
-        while n>0:
-            nums1[last] = nums2[n-1]
-            n, last = n-1, last-1
+            trav_p -= 1
