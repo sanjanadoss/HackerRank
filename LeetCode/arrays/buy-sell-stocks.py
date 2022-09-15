b1c3e8c4bae08e5ed6b97ceee0a126362d3abf3c
sanjanadoss <sanjanadoss2503@gmail.com>
2022-09-15 11:10:07 +0530

    Update buy-sell-stocks.py

--- LeetCode/arrays/buy-sell-stocks.py
+++ LeetCode/arrays/buy-sell-stocks.py
@@ -1,16 +1,20 @@
 #https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2:
-            return 0
-        maxp = 0
-        minp = prices[0]
-        for p in prices[1:]:		
-            maxp = max(maxp, p - minp)
-            minp = min(minp, p)
-
-        return maxp
-
-#Runtime: 1674 ms, faster than 31.71% of Python3 online submissions for Best Time to Buy and Sell Stock.
-#Memory Usage: 25 MB, less than 57.47% of Python3 online submissions for Best Time to Buy and Sell Stock.
+def maxProfit(self, prices):
+    """
+n--> length of array
+Time Complexity: O(n)
+Space Complexity: O(1)
+    """
+    
+    l = 0 #buy
+    r = 1 #sell
+    max_p = 0
+    
+    while r<len(prices):
+        if prices[r]>prices[l]:
+            profit = prices[r]-prices[l]
+            max_p = max(max_p, profit)
+        else:
+            l = r
+        r +=1
