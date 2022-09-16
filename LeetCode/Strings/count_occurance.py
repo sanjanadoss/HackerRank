6e9dac69034c263a110f43f3f3219a2e50833755
sanjanadoss <sanjanadoss2503@gmail.com>
2022-09-16 09:24:28 +0530

    Changes

--- LeetCode/Strings/count_occurance.py
+++ LeetCode/Strings/count_occurance.py
@@ -8,3 +8,29 @@ for i in string:
     else:
         ans_dict[i] = 1
 print(ans_dict)
+
+#counting occurance and printing them like w3s2a1
+string = "geeks for geeks"
+string = string.replace(" ", "")
+ans_dict = {}
+  
+for i in string:
+    if i in ans_dict:
+        ans_dict[i] += 1
+    else:
+        ans_dict[i] = 1
+for i in ans_dict:
+    print("{}{}".format(i, ans_dict[i]), end="")
+
+#count occurance in sorting order
+string = "geeks for geeks"
+string = string.replace(" ", "")
+ans_dict = {}
+for i in string:
+    if i in ans_dict:
+        ans_dict[i] += 1
+    else:
+        ans_dict[i] = 1
+a = sorted(ans_dict.items(), key=lambda x: x[1])
+for i in range(len(a)):
+    print("{}".format(a[i]), end="")
